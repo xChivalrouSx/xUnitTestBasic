8c349158d7b779ca262f8e899795b9b1b2292848
Mert Çakar <mertcakar.es@gmail.com>
2020-01-20 22:15:55 +0300

    Test examples are added for string assertions.

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -12,5 +12,71 @@ namespace GameEngine.Tests
 
             Assert.True(sut.IsNoob);
         }
+
+        [Fact]
+        public void CalculateFullName() 
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Mert";
+            sut.LastName = "Çakar";
+
+            Assert.Equal("Mert Çakar", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameStartWithFirstName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Mert";
+            sut.LastName = "Çakar";
+
+            Assert.StartsWith("Mert", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndWithLastName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Mert";
+            sut.LastName = "Çakar";
+
+            Assert.EndsWith("Çakar", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCaseAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "MERT";
+            sut.LastName = "ÇAKAR";
+
+            Assert.Equal("Mert Çakar", sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CalculateFullName_SubstringAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Mert";
+            sut.LastName = "Çakar";
+
+            Assert.Contains("t Ça", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Mert";
+            sut.LastName = "Çakar";
+
+            Assert.Matches("[A-ZÝÐÜÞÖÇ]{1}[a-zðüþöç]+ [A-ZÝÐÜÞÖÇ]{1}[a-zðüþöç]+", sut.FullName);
+        }
     }
 }
