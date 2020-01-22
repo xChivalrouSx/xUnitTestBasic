d006fc844c2f48c628e0ced8a151deab2d8a8b3a
Mert Çakar <mertcakar.es@gmail.com>
2020-01-22 23:35:40 +0300

    Test examples are added for floating point values.

---
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorrectPower() 
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
+        }
+    }
+}
