55e31726e89663a2c6ca600f584be614fbe81479
Mert Çakar <mertcakar.es@gmail.com>
2020-01-23 00:18:32 +0300

    Test examples are added for object types.

---
+++ GameEngine.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalEnemyByDefault() 
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+
+        [Fact]
+        public void NotCreateBossEnemyByDefault()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsNotType<BossEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsType<BossEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_CastReturnedTypeExample()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            BossEnemy boss = Assert.IsType<BossEnemy>(enemy);
+            Assert.Equal("Zombie King", boss.Name);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_AssertAssignableTypes()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsAssignableFrom<Enemy>(enemy);
+        }
+    }
+}
