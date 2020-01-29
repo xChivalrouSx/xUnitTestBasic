10fc56861e1abc77a3efbf5dd9d1a549f80417ef
Mert Çakar <mertcakar.es@gmail.com>
2020-01-29 23:38:27 +0300

    Some code are added for context sharing.

---
+++ GameEngine.Tests/GameStateShould.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class GameStateShould
+    {
+        [Fact]
+        public void DamageAllPlayersWhenEarthquake()
+        {
+            var sut = new GameState();
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            sut.Players.Add(player1);
+            sut.Players.Add(player2);
+
+            var expectedHealthAfterEarthquake = player1.Health - GameState.EarthquakeDamage;
+
+            sut.Earthquake();
+
+            Assert.Equal(expectedHealthAfterEarthquake, player1.Health);
+            Assert.Equal(expectedHealthAfterEarthquake, player2.Health);
+        }
+
+        [Fact]
+        public void Reset()
+        {
+            var sut = new GameState();
+
+            var player1 = new PlayerCharacter();
+            var player2 = new PlayerCharacter();
+
+            sut.Players.Add(player1);
+            sut.Players.Add(player2);
+
+            sut.Reset();
+
+            Assert.Empty(sut.Players);            
+        }
+    }
+}
